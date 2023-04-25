41f06bfee8a57f057e79ea8a1859bac5d2669764
Noah Crouch <ncrouch1@uw.edu>
2023-04-25 16:46:30 -0700

    lab 2 task 1 finished NC

--- Lab2/Lab2_Sketch/Lab2_Sketch.ino
+++ Lab2/Lab2_Sketch/Lab2_Sketch.ino
@@ -1,19 +1,45 @@
-#define digiwrite 1
-#ifndef digiwrite
-  #define digiwrite 0
-#endif
-
+#include "Lab2_header.h";
 
 void setup() {
-  if (digiwrite == 1) {
-    pinMode(47, OUTPUT);
-    pinMode(48, OUTPUT);
-    pinMode(49, OUTPUT);
-  }
+  if (digiwrite && !bitwrite) pinMode(47, OUTPUT); pinMode(48, OUTPUT); pinMode(49, OUTPUT);
+  if (bitwrite && !digiwrite) DDRL |= BIT_0; DDRL |= BIT_1; DDRL |= BIT_2;
 }
 
 void loop() {
-  if (digiWrite == 1) {
-    
-  }
+  if (digiwrite == 1) digiWriteBlinks();
+  if (bitwrite == 1) bitWriteBlinks();
+}
+
+void digiWriteBlinks() {
+  for (int i = 0; i < 1000; i++) {
+      if (i == 0) {
+        digitalWrite(47, HIGH);
+      } else if (i == 333) {
+        digitalWrite(47, LOW);
+        digitalWrite(48, HIGH);
+      } else if (i == 666) {
+        digitalWrite(48, LOW);
+        digitalWrite(49, HIGH);
+      }
+      delay(1);
+    }
+    digitalWrite(49, LOW);
+    delay(1);
+}
+
+void bitWriteBlinks() {
+  for (int i = 0; i < 1000; i++) {
+      if (i == 0) {
+        PORTL ^= BIT_2;
+      } else if (i == 333) {
+        PORTL ^= BIT_2;
+        PORTL ^= BIT_1;
+      } else if (i == 666) {
+        PORTL ^= BIT_1;
+        PORTL ^= BIT_0; 
+      }
+      delay(1);
+    }
+    PORTL ^= BIT_0;
+    delay(1);
 }
---
+++ Lab2/Lab2_Sketch/Lab2_header.h
@@ -0,0 +1,22 @@
+// #define digiwrite 1
+#ifndef digiwrite
+  #define digiwrite 0
+#endif
+
+#define bitwrite 1
+#ifndef bitwrite 
+  #define bitwrite 0
+#endif
+
+#define BIT_0 (1 << 0)
+#define BIT_1 (1 << 1)
+#define BIT_2 (1 << 2)
+#define BIT_3 (1 << 3)
+#define BIT_4 (1 << 4)
+#define BIT_5 (1 << 5)
+#define BIT_6 (1 << 6)
+#define BIT_7 (1 << 7)
+
+
+void digiWriteBlinks();
+void bitWriteBlinks();
