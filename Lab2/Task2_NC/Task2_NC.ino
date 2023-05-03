6e26b95d477a4678e987ea9e04abf64546698740
Noah Crouch <ncrouch1@uw.edu>
2023-05-03 10:57:39 -0700

    commit lol

--- Lab2/Task2_NC/Task2_NC.ino
+++ Lab2/Task2_NC/Task2_NC.ino
@@ -1,9 +1,11 @@
-#include "Task2.h";
-
 void setup() {
-    bit_set(DDRH, 3);
-    bit_clr(PRR1, 4);
-    
+    Serial.begin(115200);
+    DDRH |= (1 << 3);
+    PRR1 &= ~(1 << 4);
+    OCR4AH = 400;
+    TCCR4B |= (1 << 3) | (1 << 4) | (1 << 7) | (1 << 6);
+    TCCR4A |= (1 << 1) | 1;
+    Serial.println(TCCR4A, BIN);
 }
 
 void loop() {
