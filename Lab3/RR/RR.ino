094ee591ba933fb2a2c99e914a052d03afacb850
Noah Crouch <ncrouch1@uw.edu>
2023-05-18 15:28:08 -0700

    rr make me go nuts

--- Lab3/RR/RR.ino
+++ Lab3/RR/RR.ino
@@ -2,8 +2,9 @@
 #include "C:\\Users\\noah-\\Documents\\EE-CSE-474\\474_Labs_TK_NC\\Lab3\\Lab3.h";
 
 unsigned long startTime;
-int songIndex, songCount;
-unsigned long startTime2;
+int songIndex, songCount, sleeping;
+unsigned long stepTime;
+unsigned long sleepTime;
 void setup() {
     LED_DATA_DIR_REG |= BIT6;
     DATA_DIRECTION_REG_SPKR |= BIT3;
@@ -17,9 +18,8 @@ void setup() {
     TIMER_4_CTRL_REG_B |= TIMER_4_CTRL_REG_B_MASK; 
 
 
-    startTime = millis(), startTime2 = millis();
-    songIndex = 0;
-    songCount = 0;
+    startTime = millis(), stepTime = millis();
+    sleepTime = 0, songIndex = 0, songCount = 0, sleeping = 0;
 }
 
 void loop() {
@@ -38,13 +38,22 @@ void task1(unsigned long startTime) {
 }
 
 void task2(unsigned long startTime) {
-    unsigned long currTime = millis() - startTime2;
-    if (currTime > 100 && songCount < 2) {
-        TIMER_4_TOP = song[songIndex++];
-        startTime2 = millis();
+    unsigned long currTime = millis() - startTime;
+    unsigned long currTimeFromStep = millis() - stepTime;
+    if (sleepTime - millis() < 0 && sleeping == 1) {
+        songCount = 0;
+        sleeping = 0;
     }
-    if (songIndex > 21 && songCount < 2) {
+    if (currTimeFromStep > 100 && sleeping == 0) {
+        TIMER_4_TOP = song[songIndex++];
+        stepTime = millis();
+    }
+    if (songIndex > 21 && sleeping == 0) {
         songIndex = 0;
         songCount++;
+    }
+    if (songCount >= 2 && sleeping == 0) {
+        sleepTime = millis() + 4000;
+        sleeping = 1;
     }
 }
