b681d4d148300c8cd88871a7e0b21a6ecfbd1647
Taylor <taylorwkelly2020@gmail.com>
2023-05-18 16:24:41 -0700

    DDS part 1

--- Lab3/DDS/DDS_Scheduler/Lab3_DDS.h
+++ Lab3/DDS/DDS_Scheduler/Lab3_DDS.h
@@ -12,6 +12,7 @@
 #define HALTED 2
 #define SLEEPING 3
 
+#define N_TASKS 10
 
 #define LEDPORT PORTB
 #define LED_DATA_DIR_REG DDRB
@@ -21,13 +22,37 @@
 
 #define TIMER_CTRL_REG 
 
+// We recommend a duration of 100 ms per note
+#define E 659 // Hz
+#define C 523 // Hz
+#define G 784 // Hz
+#define g 392 // Hz
+#define R 0 // Rest, play no sound
+int song[] = {E, R, E, R, R, E, R, R, C, R, E, R, R, G, R, R, R, R,
+R, g, R};
+
+byte seven_seg_digits[10][7] = { 
+  { 1,1,1,1,1,1,0 },  // = 0
+  { 0,1,1,0,0,0,0 },  // = 1
+  { 1,1,0,1,1,0,1 },  // = 2
+  { 1,1,1,1,0,0,1 },  // = 3
+  { 0,1,1,0,0,1,1 },  // = 4
+  { 1,0,1,1,0,1,1 },  // = 5
+  { 1,0,1,1,1,1,1 },  // = 6
+  { 1,1,1,0,0,0,0 },  // = 7
+  { 1,1,1,1,1,1,1 },  // = 8
+  { 1,1,1,0,0,1,1 }   // = 9
+  };
 
 
-void task1(unsigned long);
-void task2(unsigned long);
-void task3(unsigned long);
-void task4(unsigned long);
-void task5(unsigned long);
+
+void task1(void *p);
+void task2(void *p);
+void task3(void *p);
+void task4_0(void *p);
+void task4_1(void *p);
+void task4_2(void *p);
+void task5(void *p);
 
 
 int compare(int, int);
