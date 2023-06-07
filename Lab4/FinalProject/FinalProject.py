67d36762bdca6fcd30325bd633e2dcb4d16d0191
Noah Crouch <ncrouch1@uw.edu>
2023-06-07 15:01:14 -0700

    comments.exe

--- Lab4/FinalProject/FinalProject.py
+++ Lab4/FinalProject/FinalProject.py
@@ -1,53 +1,126 @@
 import serial
 import openai
 
+# descriptors for the openAI api key and the serial connection
 key = "insert key here"
 sConnection = serial.Serial()
 
+"""
+    Function: waitForArduino()
+    Paramaters: None
+    Outputs: None
+    Purpose: Waits for the arduino to initiate a pairing process,
+    once it is paired it will communicate that to the arduino
+"""
 def waitForArduino():
+    # loop forever
     while True:
+        # print to the terminal that we are waiting
         print('waiting')
+        # read the line
         str = sConnection.readline()
+        # decode and remove white space
         str = str.decode().strip()
+        # if the string is the requesting pair string break the loop
         if str == "Requesting Pair":
             break
+    # Writeback that we have successfully paired
     sConnection.write(b'Paired\n')
 
+"""
+    Function: sendToArduino(sendStr)
+    Parameters: sendStr - String to send
+    Outputs: None
+    Purpose: Send the data back to the arduino
+"""
 def sendToArduino(sendStr):
-    sendStr.replace('\"', "")
+    # remove quotation marks if needed
+    sendStr.replace('"', "")
+    # Write to the arduino the string to be sent
     sConnection.write((sendStr+'\n').encode())
+    # write to the arduino that we are done
     sConnection.write(b'Done\n')
+    # print to the terminal that we have sent 
+    # the data to the terminal
     print('sent')
 
+"""
+    Function: receiveFromArduino()
+    Parameters: None
+    Outputs: str - string of tokens received from arduino
+    Purpose: Get the data from the arduino
+"""
 def receiveFromArduino():
+    # print that we are receiving data
     print('receiving')
+    # initiate an empty string
     str = ""
+    # loop forever
     while True:
+        # read a line from the serial connection
         nextline = sConnection.readline()
+        # decode and strip the whitespace from the line
         nextline = nextline.decode().strip()
+        # if the string is the done command, break
         if nextline == "Done":
             break
+        # else concatenate the information then move one
         str += nextline
-    if str == "Done":
-        raise ValueError("Prompt invalid")
+    # if the string is empty something has gone wrong
+    if str == "":
+        # Raise error that we didnt receive anything
+        raise ValueError("Empty string received instead of tokens")
+    # return the string
     return str
 
+"""
+    Function: promptGPT(str)
+    Parameters: str - string of tokens to be inserted into the prompt template
+    Output: Result of the prompt
+    Purpose: Prompt chatGPT with the tokens from the aruino to make a string about
+    a person
+"""
 def promptGPT(str):
+    # Assign the key
     openai.api_key = key
+    # append the tokens to the prompt template
     promptstr = "Please make a sentence to someone you are talking to using these words: " + str
+    # construct the prompt
     prompt = [{"role": "user", "content": promptstr}]
+    # send the prompt to chatGPT and receive the response
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt)
+    # get the content of the response
     result = response['choices'][0]['message']['content']
+    # return the result
     return result
+
+"""
+    Function: program()
+    Parameters: None
+    Outputs: None
+    Purpose: Facilitates the process of opening the serial port,
+    waiting for the arduino, receiving from the arduino, prompting chatGPT,
+    sending the data to the arduino, then closing the port
+"""
 def program():
+    # set the port to COM5
     sConnection.port = 'COM5'
+    # set the baudrate to 19200
     sConnection.baudrate = 19200
+    # open the port
     sConnection.open()
+    # wait for arduino pair
     waitForArduino()
+    # get data from arduino
     prompt = receiveFromArduino()
+    # send prompt to gpt and get response
     response = promptGPT(prompt)
+    # send the data back to the arduino
     sendToArduino(response)
+    # clsoe the port
     sConnection.close()
 
+
+# Program entry point
 if __name__ == '__main__':
     program()
--- Lab4/FinalProject/Lab4.h
+++ Lab4/FinalProject/Lab4.h
@@ -1,11 +1,15 @@
+// Include block to make sure that code isnt being defined more
+// than once
 #ifndef LAB4_H
 #define LAB4_H
 
+// Include all necessary header files
 #include <Arduino_FreeRTOS.h>
 #include <LiquidCrystal.h>
 #include <arduinoFFT.h>
-// #include <ChatGPT.hpp>
 
+// Pin definitions for code clarity
+// pins 31 - 22, used for the LCD
 #define PIN31 31
 #define PIN30 30
 #define PIN29 29
@@ -17,24 +21,24 @@
 #define PIN23 23
 #define PIN22 22
 
+// Pins 12 - 8 used for the LEDs
 #define PIN12 12
 #define PIN11 11
 #define PIN10 10
 #define PIN9 9
 #define PIN8 8
-#define PIN7 7
-#define PIN6 6
-#define PIN5 5
 
+// Pins A0 and A1, used with the joystick
 #define XVALUEPIN A0
 #define YVALUEPIN A1
 
-
-
+// String definitions used for string generation, these ones are not formatted for the LCD and are used exclusively
+// in the generation of the string
 char* adjectives[91] = {
     "Adventurous ", "Amazing ", "Amiable ", "Awesome ", "Balanced ", "Blissful ", "Brilliant ", "Caring ", "Cheerful ", "Creative ", "Dazzling ", "Delightful ", "Dynamic ", "Eager ", "Effervescent ", "Enthusiastic ", "Faithful ", "Fearless ", "Friendly ", "Fulfilled ", "Gentle ", "Genuine ", "Gracious ", "Grateful ", "Happy ", "Harmonious ", "Heartfelt ", "Innovative ", "Inspiring ", "Jolly ", "Joyful ", "Joyous ", "Jubilant ", "Keen ", "Kind ", "Kindhearted ", "Kindred ", "Liberal ", "Lively ", "Loving ", "Lucky ", "Magnificent ", "Modest ", "Motivating ", "Motivated ", "Natural ", "Nifty ", "Noble ", "Nurturing ", "Optimal ", "Optimistic ", "Original ", "Passionate ", "Patient ", "Peaceful ", "Positive ", "Quaint ", "Quick ", "Quick-witted ", "Quirky ", "Radiant ", "Rejuvenating ", "Reliable ", "Resilient ", "Satisfied ", "Sincere ", "Spirited ", "Sunny ", "Talented ", "Thoughtful ", "Thriving ", "Tranquil ", "Understanding ", "Unstoppable ", "Upbeat ", "Upstanding ", "Valuable ", "Versatile ", "Vibrant ", "Vivacious ", "Wholesome ", "Wise ", "Wonderful ", "Wondrous ", "Xenial ", "Xtraordinary ", "Yearning ", "Youthful ", "Zealotic ", "Zealous ", "Zestful "
 };
 
+// String definitions used for the LCD, these ones are padded to length 16 for use in the LCD
 char* adjectives_lcd[91] = {
     "Adventurous    ",
 "Amazing        ",
@@ -129,6 +133,5 @@ char* adjectives_lcd[91] = {
 "Zestful        "
 };
 
-
-
+// end include block
 #endif
